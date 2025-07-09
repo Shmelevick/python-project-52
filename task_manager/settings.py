08c584605991e0ab0c37d4f9ae1c4076dba8ddbe
shmelevik <yaroslav1992@gmail.com>
2025-07-09 13:21:00 +0300

    1.4

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -30,7 +30,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['webserver', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'webserver',
+    'localhost',
+    '127.0.0.1',
+    'https://python-project-52-keai.onrender.com/',
+]
 
 
 # Application definition
